1a2878a2d5ac87a4f1c0ae01fc7c19ec00be8418
Edwin Hernández <edwinhdevelop@gmail.com>
2025-07-07 00:14:43 -0400

    mas productos

--- src/data/item-data.ts
+++ src/data/item-data.ts
@@ -51,4 +51,134 @@ export const items: Item[] = [
     image: "/images/items/esmeril/D_NQ_NP_2X_938717-MLV75631737101_042024-F.webp",
     category: "Herramientas"
   },
+  {
+    id: "6",
+    name: "Generador planta eléctrica Duromax Dual 4850w gas/ gasolina",
+    slug: "generador-planta-electrica-duromax-dual-4850w-gas-gasolina",
+    description: "Generador planta eléctrica Duromax Dual 4850w gas/ gasolina",
+    price: "650",
+    discount: 0,
+    image: "/images/items/generador1/D_NQ_NP_636856-MLV80279128065_102024-O.webp",
+    category: "Generadores Electricos"
+  },
+  {
+    id: "7",
+    name: "Planta electrica 1200 watts generador, 110v",
+    slug: "planta-electrica-1200-watts-generador-110v",
+    description: "Planta electrica 1200 watts generador, 110v",
+    price: "180",
+    discount: 0,
+    image: "/images/items/generador2/D_NQ_NP_2X_897664-MLV76911691787_062024-F.webp",
+    category: "Generadores Electricos"
+  },
+  {
+    id: "8",
+    name: "Hidrojet de 1400w 1500psi 110v Roccia",
+    slug: "hidrojet-de-1400w-1500psi-110v-rocchia",
+    description: "Hidrojet de 1400w 1500psi 110v Roccia",
+    price: "95,70",
+    discount: 0,
+    image: "/images/items/hidrojet/D_NQ_NP_2X_950731-MLV75102104943_032024-F.webp",
+    category: "Herramientas"
+  },
+  {
+    id: "9",
+    name: "Hidrojet 1800psi 1500w compacta marca Tegatok",
+    slug: "hidrojet-1800psi-1500w-compacta-marca-tegatok",
+    description: "Hidrojet 1800psi 1500w compacta marca Tegatok",
+    price: "94,60",
+    discount: 0,
+    image: "/images/items/hidrojet2/D_NQ_NP_2X_813829-MLV73897160161_012024-F.webp",
+    category: "Herramientas"
+  },
+  {
+    id: "10",
+    name: "Rollo manguera jardín culebra resistente reforzada 1/2 15mts",
+    slug: "rollo-manguera-jardin-culebra-resistente-reforzada-1-2-15mts",
+    description: "Rollo manguera jardín culebra resistente reforzada 1/2 15mts",
+    price: "17,86",
+    discount: 0,
+    image: "/images/items/mangueras1/D_NQ_NP_2X_610830-MLV50893749817_072022-F.webp",
+    category: "Mangueras de Riego"
+  },
+  {
+    id: "11",
+    name: "Mangueras de riego 3/8 1/2 3/4 1 11/4 11/2 2 pulgadas",
+    slug: "mangueras-de-riego-3-8-1-2-3-4-1-11-4-11-2-2-pulgadas",
+    description: "Mangueras de riego 3/8 1/2 3/4 1 11/4 11/2 2 pulgadas",
+    price: "20",
+    discount: 0,
+    image: "/images/items/mangueras2/D_NQ_NP_2X_616006-MLV76642660594_062024-F.webp",
+    category: "Mangueras de Riego"
+  },
+  {
+    id: "12",
+    name: "Motobomba autocebante 2 pulgadas Sol Power",
+    slug: "motobomba-autocebante-2-pulgadas-sol-power",
+    description: "Motobomba autocebante 2 pulgadas Sol Power",
+    price: "200",
+    discount: 0,
+    image: "/images/items/motobomba1/D_NQ_NP_819892-MLV82422962374_022025-O.webp",
+    category: "Motobomba"
+  },
+  {
+    id: "13",
+    name: "Motobomba autocebante Genpar 7 Hp, 2x2 Motor A Gasolina",
+    slug: "motobomba-autocebante-genpar-7-hp-2x2-motor-a-gasolina",
+    description: "Motobomba autocebante Genpar 7 Hp, 2x2 Motor A Gasolina",
+    price: "199",
+    discount: 0,
+    image: "/images/items/motobomba2/D_NQ_NP_731777-MLV70742168989_072023-O.webp",
+    category: "Motobomba"
+  },
+  {
+    id: "14",
+    name: "Motosierra eléctrica recargable doble batería 48v estuche",
+    slug: "motosierra-electrica-recargable-doble-bateria-48v-estuche",
+    description: "Motosierra eléctrica recargable doble batería 48v estuche",
+    price: "85",
+    discount: 0,
+    image: "/images/items/motosierra1/D_NQ_NP_2X_867197-MLV80136561193_102024-F.webp",
+    category: "Motosierras"
+  },
+  {
+    id: "15",
+    name: "Mini serrucho motosierra inalámbrica portátil recargable 48v",
+    slug: "mini-serrucho-motosierra-inalambrica-portatil-recargable-48v",
+    description: "Mini serrucho motosierra inalámbrica portátil recargable 48v",
+    price: "38",
+    discount: 0,
+    image: "/images/items/motosierra2/D_NQ_NP_860722-MLV79659114599_092024-O.webp",
+    category: "Motosierras"
+  },
+  {
+    id: "16",
+    name: "Pala truper cuadrada t-1000 puño y pcy-pe",
+    slug: "pala-truper-cuadrada-t-1000-puño-y-pcy-pe",
+    description: "Pala truper cuadrada t-1000 puño y pcy-pe",
+    price: "16",
+    discount: 0,
+    image: "/images/items/pala/D_NQ_NP_2X_832420-MLV81491064829_122024-F.webp",
+    category: "Herramientas"
+  },
+  {
+    id: "17",
+    name: "Taladro percutor inalambrico 20v 1.5amp 45n.m marca Tegatok",
+    slug: "taladro-percutor-inalambrico-20v-1-5amp-45n-m-marca-tegatok",
+    description: "Taladro percutor inalambrico 20v 1.5amp 45n.m marca Tegatok",
+    price: "60",
+    discount: 0,
+    image: "/images/items/taladro/D_NQ_NP_2X_619532-MLV74136544660_012024-F.webp",
+    category: "Herramientas"
+  },
+  {
+    id: "18",
+    name: "Taladro percutor profesional 810watts 1/2 110volt ingco",
+    slug: "taladro-percutor-profesional-810watts-1-2-110volt-ingco",
+    description: "Taladro percutor profesional 810watts 1/2 110volt ingco",
+    price: "48,86",
+    discount: 0,
+    image: "/images/items/taladro3/D_NQ_NP_2X_712453-MLV75596076092_042024-F.webp",
+    category: "Herramientas"
+  },
 ];
